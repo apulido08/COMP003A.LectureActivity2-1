dc9008c0c93bed67f696eb98da70702a7fbd496f
adodoCoder <alavropulido@whccd.edu>
2025-01-21 19:23:55 -0800

    added last set of instructions to code

--- COMP003A.LectureActivity2-1/Program.cs
+++ COMP003A.LectureActivity2-1/Program.cs
@@ -36,6 +36,14 @@ namespace COMP003A.LectureActivity2_1
             bool isAdult = userAge >= 18; // Determine if the user is 18 or older
             bool isAdultStudent = isStudent && isAdult; // Determine if the user is both a student and an adult
 
+
+            // Step 3.3: Display outputs using string interpolation
+            Console.WriteLine($"\nHello, {userName}!"); // Greet the user with their name
+            Console.WriteLine($"You are currently {userAge} years old. In 5 years, you will be {futureAge}."); // Display current and future age
+            Console.WriteLine($"Are you 18 or older? {isAdult}"); // Display uf the user is 18 or older
+            Console.WriteLine($"Are you both a student and 18 or older? {isAdultStudent}"); // Disdplay if the user is both a student and an adult
+
+
         }
     }
 }
